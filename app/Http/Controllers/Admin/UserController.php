2a7cbf7fa591bd075ab4e57b247011aaba98fbff
chiehjie <530239095@qq.com>
2016-10-16 13:22:52 +0800

    aaaa

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -9,20 +9,6 @@ use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    public function adminUser (Request $request)
-    {
-    	$db = \DB::table('admin');
-        $where = [];
-        if($request->has('adminname')){
-            $name = $request->input('adminname');
-            $db->where('adminname', 'like', "%{$name}%");//实现过滤控制器
-            $where['name'] = $name;
-         // 模板显示
-        } 
-        // dd($where);
-        $list = $db->paginate(3);
-        return view("admin.adminUser.user")->with(['list'=>$list,"where"=>$where]);
-    }
     public function index2 (Request $request)
     {
     	// 获得一个连接对象
