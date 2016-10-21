308a41f1617cbab1ade07fe05109c9223421d9e0
hql <wj530239095@qq.com>
2016-10-21 15:27:37 +0800

    Merge origin/master
    Conflicts:
    	app/Http/Controllers/AjaxController.php
    	resources/views/admin/base/base.blade.php
    	resources/views/admin/type/order.blade.php
    	routes/web.php

--- app/Http/Controllers/ZhiFuController.php
+++ app/Http/Controllers/ZhiFuController.php
@@ -8,9 +8,9 @@ use App\Http\Requests;
 
 class ZhiFuController extends Controller
 {
-    public function zhifu()
+    public function zhifu($id)
     {	
-		$uid = session('adminuser')->id;
+    	$uid = session('adminuser')->id;
 		// 用户详情
 		$data = \DB::table('user_c')->where('uid',$uid)->first();
 		// 用户购物车
@@ -18,7 +18,7 @@ class ZhiFuController extends Controller
 		$time = time();
 		\DB::table('details')->insert(['uid'=>$uid,'gid'=>$list->gid,'color'=>$list->color,'gimage'=>$list->goodsimg,'area'=>$data->area,'big'=>$list->sizes,'count'=>$list->count,'username'=>$data->username,'userphone'=>$data->phone,'status'=>'0','time'=>$time,'price'=>$list->goodsprice,'gdname'=>$list->goodname]);
     	 $configs = \DB::table('config')->get();
-
-    	return view('home.zhifu')->with(['configs'=>$configs,'list'=>$list,'data'=>$data]);
+            $wan=\DB::table('links')->get();
+    	return view('home.zhifu')->with(['configs'=>$configs,'list'=>$list,'data'=>$data,'wan'=>$wan]);
     }
 }
